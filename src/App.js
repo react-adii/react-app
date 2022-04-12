53ebc73bad4739879e1e3c0b231658de5fa24ed2
react-adii <qasim.afzal@capslock.pk>
2022-04-12 14:33:29 +0500

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am university Student.
+         Have a Nice Day!
         </a>
       </header>
     </div>
